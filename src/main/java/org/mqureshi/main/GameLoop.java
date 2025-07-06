dc8f663b900ad7a49125b6aed5b0c3833d198a33
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2025-07-06 23:04:08 +0100

    feat: clean up main class

--- src/main/java/org/mqureshi/main/GameLoop.java
+++ src/main/java/org/mqureshi/main/GameLoop.java
@@ -33,8 +33,8 @@ public class GameLoop {
 
         Light light = new Light(new Vector3f(3000, 2000, 20), new Vector3f(1, 1, 1));
 
-        Terrain terrain = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
-        Terrain terrain2 = new Terrain(-1, 0, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
+        Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
+        Terrain terrain2 = new Terrain(-1, -1, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
 
         Camera camera = new Camera(displayManager.getWindowHandle());
         MasterRenderer renderer = new MasterRenderer(displayManager);
